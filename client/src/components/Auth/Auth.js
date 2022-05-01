ae97cf3bbfa3e4b1288dc61e010e4add2ddc71b9
georgepap9808 <el17836@mail.ntua.gr>
2022-05-01 23:35:01 +0300

    first commit for client

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Button, Paper, Grid, Typography, Container} from "@material-ui/core"
+// import LockTwoToneIcon from '@mui/icons-material/LockTwoTone';
+import useStyles from "./styles";
+import Input from "./input";
+
+// const Auth = () => {
+//     return (
+//         <div>
+//             <h1>AUTH</h1>
+//         </div>
+//     )
+// }
+const Auth = () => {
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleShowPassword = () => setShowPassword(!showPassword);
+    const handleSubmit = () => {
+
+    };
+    const handleChange = () => {
+
+    };
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        setShowPassword(false);
+    };
+
+    return (
+        <Container component={"main"} maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                {/* <Avatar className={classes.avatar}>
+                    <LockTwoToneIcon />
+                </Avatar> */}
+                <Typography variant="h5">{ isSignup ? 'Sign Up' : 'Sign in'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                                </>
+                            )
+                        }
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        { isSignup ? 'Sign Up' : 'Sign In' }
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                        <Button onClick={switchMode}>
+                            { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+                        </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Saas 2022 Project Homepage</h1>
+        </div>
+    )
+}
+
+export default Home;
